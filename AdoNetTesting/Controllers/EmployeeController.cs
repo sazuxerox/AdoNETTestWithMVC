2f86c3e581037584cecf9397810e827ef1e85eca
sazuxerox <sazzadhossain@live.com>
2015-06-12 19:04:09 +0600

    ado dot net testing with mvc

---
+++ AdoNetTesting/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+
+namespace AdoNetTesting.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var employeeBusinessLayer = new EmployeeBusinessLayer();
+            var employees = employeeBusinessLayer.Employees.ToList();
+
+            return View(employees);
+        }
+	}
+}
---
+++ BusinessLayer/EmployeeBusinessLayer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class EmployeeBusinessLayer
+    {
+        public IEnumerable<Employee> Employees
+        {
+            get
+            {
+                var connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+               
+                var employees = new List<Employee>();
+
+                using (var con = new SqlConnection(connectionString))
+                {
+                    
+                    var cmd = new SqlCommand("getAllEmployees", con) { CommandType = CommandType.StoredProcedure };
+                    con.Open();
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var employee = new Employee
+                        {
+                            EmployeeId = Convert.ToInt32(reader["EmployeeId"]),
+                            Name = reader["Name"].ToString(),
+                            Gender = reader["Gender"].ToString(),
+                            City = reader["City"].ToString(),
+                            DateOfBirth = Convert.ToDateTime(reader["DateOfBirth"])
+                        };
+
+                        employees.Add(employee);
+                    }
+
+                }
+                return employees;
+            }
+        }
+
+     }
+}
